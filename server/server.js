db781d086dd31a026595f52f45aefb811499a2c8
AdagiAvoga <dinahavoga@gmail.com>
2025-07-30 15:59:11 +0300

    Full project update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+app.use(cors( {
+    origin: 'https://academix-mern.vercel.app'
+})) 
+
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/authRoutes'))
