176fec61453016d7a276ba4a57978c55076545be
AdagiAvoga <dinahavoga@gmail.com>
2025-07-31 11:40:12 +0300

    Updated baseURL to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://academix-backend-o374.onrender.com',
+    baseURL: 'https://academix-backend-o374.onrender.com/api',
     withCredentials: false
 })
 
